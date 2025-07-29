bbe86fce89e2349afdc3a044f5c4d8976139bdae
adriantlh <adrian.tan.li.huat@gmail.com>
2025-07-29 13:10:09 +0800

    uploading layouts

--- app/src/main/java/team2/kakigowhere/MainActivity.kt
+++ app/src/main/java/team2/kakigowhere/MainActivity.kt
@@ -7,14 +7,45 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Load the default fragment (e.g., Home)
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, DetailFragment())
+            .commit()
+
+        // Set up custom bottom nav bar interactions
+        findViewById<LinearLayout>(R.id.nav_home).setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, HomeFragment())
+                .commit()
+        }
+
+        findViewById<LinearLayout>(R.id.nav_map).setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, MapFragment())
+                .commit()
+        }
+
+        findViewById<LinearLayout>(R.id.nav_explore).setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, ExploreFragment())
+                .commit()
+        }
+
+        findViewById<LinearLayout>(R.id.nav_saved).setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, SavedFragment())
+                .commit()
+        }
+
+        findViewById<LinearLayout>(R.id.nav_profile).setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, ProfileFragment())
+                .commit()
         }
     }
 }
