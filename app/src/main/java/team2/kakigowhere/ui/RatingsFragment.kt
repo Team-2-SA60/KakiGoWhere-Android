6d5d8cfa3cbc0b73bcc93ccce4371217a3e146ce
gongyuannn <104050074+gongyuannn@users.noreply.github.com>
2025-08-05 02:14:41 +0800

    Amend ratings fragment (temporarily parked it under Profile for testing) - display comments based on hardcoded tourist id and place id

--- app/src/main/java/team2/kakigowhere/ui/RatingsFragment.kt
+++ app/src/main/java/team2/kakigowhere/ui/RatingsFragment.kt
@@ -5,19 +5,14 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import team2.kakigowhere.data.api.RetrofitClient
-import team2.kakigowhere.data.model.RatingItem
 import team2.kakigowhere.adapters.RatingsAdapter
-import team2.kakigowhere.data.api.ApiService
 import team2.kakigowhere.databinding.FragmentRatingsBinding
 
 class RatingsFragment : Fragment() {
