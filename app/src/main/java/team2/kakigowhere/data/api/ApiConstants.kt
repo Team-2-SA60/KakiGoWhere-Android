32f0b1b965b60e447d82b7afa37650520f8963f6
vegecloud <caiyun_yap@hotmail.com>
2025-08-10 19:42:57 +0800

    base url to prod.

--- app/src/main/java/team2/kakigowhere/data/api/ApiConstants.kt
+++ app/src/main/java/team2/kakigowhere/data/api/ApiConstants.kt
@@ -1,8 +1,7 @@
 package team2.kakigowhere.data.api
 
 object ApiConstants {
-    //const val BASE_URL = "http://206.189.43.202:8080/api/"
-    const val BASE_URL = "http://10.0.2.2:8080/api/"
+    const val BASE_URL = "http://206.189.43.202:8080/api/"
 
     const val ML_BASE_URL = "http://10.0.2.2:5001/"
 
