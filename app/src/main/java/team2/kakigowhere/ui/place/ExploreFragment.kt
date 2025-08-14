574845555970de3f9e7ea379c57b5bb0cccc5c97
Bofei2058 <dorrr2058@outlook.com>
2025-08-14 16:49:52 +0800

    refresh btn update

--- app/src/main/java/team2/kakigowhere/ui/place/ExploreFragment.kt
+++ app/src/main/java/team2/kakigowhere/ui/place/ExploreFragment.kt
@@ -35,29 +35,18 @@ class ExploreFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.loadingOverlay.visibility = View.VISIBLE
-
-        // Fetch PlaceDTOs
-        lifecycleScope.launch {
-            val resp = RetrofitClient.api.getPlaces()
-            originalPlaces = resp.body() ?: emptyList<PlaceDetailDTO>()
-
-            currentFilteredPlaces = originalPlaces
-
-            withContext(Dispatchers.Main) {
-                setupRecycler(currentFilteredPlaces.sortedBy { it.name })
-                binding.loadingOverlay.visibility = View.GONE
-            }
-        }
+        fetchPlaces()
 
         binding.sortDefault.setOnClickListener {
             currentFilteredPlaces = currentFilteredPlaces.sortedBy { it.name }
             setupRecycler(currentFilteredPlaces)
         }
+
         binding.sortRating.setOnClickListener {
             currentFilteredPlaces = currentFilteredPlaces.sortedByDescending { it.averageRating }
             setupRecycler(currentFilteredPlaces)
         }
+
         binding.searchButton.setOnClickListener {
             val q = binding.searchInput.text.toString().trim()
             if (q.isEmpty()) {
@@ -72,11 +61,11 @@ class ExploreFragment : Fragment() {
                 setupRecycler(currentFilteredPlaces.sortedBy { it.name })
             }
         }
+
         binding.refreshButton.setOnClickListener {
             binding.searchInput.text?.clear()
             binding.tvNoResult.visibility = View.GONE
-            currentFilteredPlaces = originalPlaces
-            setupRecycler(currentFilteredPlaces.sortedBy { it.name })
+            fetchPlaces()
         }
     }
 
@@ -93,6 +82,22 @@ class ExploreFragment : Fragment() {
             adapter = this@ExploreFragment.adapter
         }
         binding.tvNoResult.visibility = View.GONE
+    }
+
+    private fun fetchPlaces() {
+        binding.loadingOverlay.visibility = View.VISIBLE
+
+        lifecycleScope.launch {
+            val resp = RetrofitClient.api.getPlaces()
+            originalPlaces = resp.body() ?: emptyList()
+
+            currentFilteredPlaces = originalPlaces
+
+            withContext(Dispatchers.Main) {
+                setupRecycler(currentFilteredPlaces.sortedBy { it.name })
+                binding.loadingOverlay.visibility = View.GONE
+            }
+        }
     }
 
     override fun onDestroyView() {
