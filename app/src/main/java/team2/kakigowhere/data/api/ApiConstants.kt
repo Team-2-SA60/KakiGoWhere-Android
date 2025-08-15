ff647b643487bab3b9d71471c606d6d60daa0fa8
kinsengchan <chan_kin_seng@hotmail.com>
2025-08-15 11:59:40 +0800

    chore: add instructions readme

--- app/src/main/java/team2/kakigowhere/data/api/ApiConstants.kt
+++ app/src/main/java/team2/kakigowhere/data/api/ApiConstants.kt
@@ -1,7 +1,8 @@
 package team2.kakigowhere.data.api
 
 object ApiConstants {
-    const val BASE_URL = "http://206.189.43.202:8080/api/"
-    const val ML_BASE_URL = "http://206.189.43.202:5001/"
-    const val IMAGE_URL = "http://206.189.43.202:8080/api/places/image/"
+    const val URL = "http://10.0.2.2"
+    const val BASE_URL = "$URL:8080/api/"
+    const val ML_BASE_URL = "\"$URL:5001/"
+    const val IMAGE_URL = "$URL:8080/api/places/image/"
 }
