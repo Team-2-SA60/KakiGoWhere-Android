d191535574e327bbde2fed7bc1440be1cb76e324
kinsengchan <chan_kin_seng@hotmail.com>
2025-08-20 10:12:06 +0800

    fix: removed "/" from ML_BASE_URL

--- app/src/main/java/team2/kakigowhere/data/api/ApiConstants.kt
+++ app/src/main/java/team2/kakigowhere/data/api/ApiConstants.kt
@@ -3,6 +3,6 @@ package team2.kakigowhere.data.api
 object ApiConstants {
     const val URL = "http://10.0.2.2"
     const val BASE_URL = "$URL:8080/api/"
-    const val ML_BASE_URL = "\"$URL:5001/"
+    const val ML_BASE_URL = "$URL:5001/"
     const val IMAGE_URL = "$URL:8080/api/places/image/"
 }
